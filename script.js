e5f63b9fb6f9cf923510ee0341d379270c6f14b8
FiutekJR <kall3rmoon@gmail.com>
2024-03-06 19:43:43 +0300

    Update #5

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function updateTime() {
     const now = new Date();
-    const days = ['Воскресенье', 'Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота'];
+    const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
     const dayOfWeek = days[now.getDay()];
     const date = now.getDate();
     const hours = now.getHours();
